fd8ac0b99557f296104d8591b7f2067a21e639da
bingshan1999 <bingshan1999@gmail.com>
2019-11-18 17:07:54 +0800

    Added tweepy dependency, and twitter authentication

--- twitter_analysis/analyze_tweets/views.py
+++ twitter_analysis/analyze_tweets/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .twitter_cred import consumer_key, consumer_secret, access_token, access_token_secret
+import tweepy
+
+#twitter authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 # Create your views here.
